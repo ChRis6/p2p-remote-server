818189a1b71203cb0ddb95a6b138d48987e067ac
Christos Papaioannou <chr6.pap@gmail.com>
2014-01-06 16:23:20 +0200

    Initial Commit

---
+++ src/group_list.c
@@ -0,0 +1,157 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "group_list.h"
+
+/**
+ * Makes an empty list of members
+ *
+ * Returns: pointer to an empty list
+ *			NULL when out of memory
+ */
+MEMBER_LIST* create_member_list(){
+
+	MEMBER_LIST* list;
+
+	list = (MEMBER_LIST*) malloc( sizeof(MEMBER_LIST));
+	if( !list )
+		return NULL;
+
+	list->head = NULL;
+	list->n = 0;
+
+	return list;
+}
+
+
+/**
+ * Make a member with a specified ip address and port number
+ * 
+ * Returns : 1) A pointer to a newly created member
+ *					or
+ *			 2) Null when out of memory
+ */
+MEMBER_T* create_member( char* ip , short port){
+
+	MEMBER_T* new_member;
+
+	if( !ip  ||  strlen(ip) != INET_ADDRSTRLEN )
+		return NULL;
+
+	new_member = ( MEMBER_T*) malloc( sizeof(MEMBER_T));
+	if( !new_member)
+		return NULL;
+
+	strcpy(new_member->ip , ip);
+	new_member->port = port;
+	new_member->next = NULL;
+
+	return new_member;
+}
+
+/**
+ * Inserts a member to the members list
+ * 
+ * Returns: 1 when successful
+ *			2 when failed
+ */
+
+int add_member( MEMBER_LIST* list , MEMBER_T* member){
+
+	if( !list || !member)
+		return 0;
+
+	if( list->n == 0 ){
+		list->head = member;
+		list->n = 1;
+	}
+	else{
+		member->next = list->head;
+		list->head = member;
+		list->n = list->n + 1 ;
+	}
+
+	return 1;
+}
+
+/**
+ * Print members on screen.Can be easily modified to
+ * print on files
+ */
+void print_member_list(MEMBER_LIST* list){
+
+
+}
+
+
+
+/**
+ * Makes an empty list of groups
+ *
+ * Returns: pointer to an empty list
+ *			NULL when out of memory
+ */
+GROUP_LIST* create_group_list(){
+
+	GROUP_LIST* list;
+
+	list = (GROUP_LIST*) malloc( sizeof(GROUP_LIST));
+	if( !list)
+		return NULL;
+
+	list->head = NULL;
+	list->n = 0; 
+
+	return list;
+
+}
+
+
+/**
+ * Makes a group with a specified name and list of members
+ * 
+ * Returns : 1) A pointer to a newly created group
+ *					or
+ *			 2) Null when out of memory
+ */
+GROUP_T* create_group(char* name , MEMBER_LIST* member_list){
+
+	GROUP_T* group = NULL;
+	if( !name || member_list)
+		return NULL;
+
+	group = ( GROUP_T*) malloc( sizeof(GROUP_T));
+	if( !group)
+		return NULL;
+
+	group->group_name = strdup(name);
+	group->members = member_list;
+	group->next = NULL;
+
+	return group;
+}
+
+/**
+ * Adds a group to the group list
+ * 
+ * Returns: 1 when successful
+ *			2 when failed
+ */
+int add_group( GROUP_LIST* list , GROUP_T* group){
+
+	if( !list || !group )
+		return 0;
+
+	if( list->n == 0 ){
+		list->head = group;
+		list->n = 1;
+	}
+	else {
+		group->next = list->head;
+		list->head = group;
+		list->n = list->n + 1;
+
+	}
+	return 1;
+}
---
+++ src/group_list.h
@@ -0,0 +1,115 @@
+#ifndef GROUP_LIST_H
+#define GROUP_LIST_H
+
+#include <arpa/inet.h>
+
+
+/* Member object */
+typedef struct member {
+	
+	/* IP address */
+	char ip[INET_ADDRSTRLEN];
+	/* port number */
+	short port;
+
+	struct member* next;
+}MEMBER_T;
+
+/* List of members */
+typedef struct member_list{
+	/* pointer to first member */
+	MEMBER_T* head;
+	/* number of members on the list */
+	unsigned int n;
+}MEMBER_LIST;
+
+
+/* p2p group object */
+typedef struct group{
+	/* p2p group name */
+	char* group_name;
+	/* list of members in the group */
+	MEMBER_LIST* members;
+
+	struct group* next;
+
+}GROUP_T;
+
+
+typedef struct group_list{
+	/* pointer to first group */
+	GROUP_T* head;
+	/* number of groups */
+	unsigned int n;
+}GROUP_LIST;
+
+
+/* ============ MEMBER MANAGEMENT ==============*/
+
+/**
+ * Makes an empty list of members
+ *
+ * Returns: pointer to an empty list
+ *			NULL when out of memory
+ */
+MEMBER_LIST* create_member_list();
+
+
+/**
+ * Makes a member with a specified ip address and port number
+ * 
+ * Returns : 1) A pointer to a newly created member
+ *					or
+ *			 2) Null when out of memory
+ */
+MEMBER_T* create_member( char* ip , short port);
+
+/**
+ * Inserts a member to the members list
+ * 
+ * Returns: 1 when successful
+ *			2 when failed
+ */
+
+int add_member( MEMBER_LIST* list , MEMBER_T* member);
+
+
+/**
+ * Print members on screen.Can be easily modified to
+ * print on files
+ */
+void print_member_list(MEMBER_LIST* list);
+
+
+
+
+/*======== GROUP MANAGEMENT ===============*/
+
+
+/**
+ * Makes an empty list of groups
+ *
+ * Returns: pointer to an empty list
+ *			NULL when out of memory
+ */
+GROUP_LIST* create_group_list();
+
+
+/**
+ * Makes a group with a specified name and list of members
+ * 
+ * Returns : 1) A pointer to a newly created group
+ *					or
+ *			 2) Null when out of memory
+ */
+GROUP_T* create_group(char* name , MEMBER_LIST* member_list);
+
+
+/**
+ * Adds a group to the group list
+ * 
+ * Returns: 1 when successful
+ *			2 when failed
+ */
+int add_group( GROUP_LIST* list , GROUP_T* group);
+#endif
